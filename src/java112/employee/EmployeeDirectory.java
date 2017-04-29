8ba5e3d369c7bc3f0d24dc6c288b5935d69640ba
Eric Knapp <eknapp@madisoncollege.edu>
2017-04-28 22:39:16 -0500

    Employee search by id, first, and last name done. Still need refactoring.

--- src/java112/employee/EmployeeDirectory.java
+++ src/java112/employee/EmployeeDirectory.java
@@ -13,9 +13,6 @@ to database.
  */
 public class EmployeeDirectory {
     
-    private Connection connection = null;
-    private Statement statement = null;
-    private ResultSet resultSet = null;
     private Properties properties;
     
     /**
@@ -34,73 +31,27 @@ public class EmployeeDirectory {
         this.properties = properties;
     }
     
-    public void processQuery() {
-  
+    public Object searchEmployeeDatabase(String searchTerm, String searchType) {
         
-  
-        try {
-            statement = connection.createStatement();
-  
-            String name = "Smith";
-            String queryString = "SELECT emp_id, first_name, last_name"
-                    + " FROM employees " + "WHERE last_name like '"
-                    + name + "%'";
-  
-            System.out.println("queryString: " + queryString);
-  
-            resultSet = statement.executeQuery(queryString);
-  
-            System.out.println();
-  
-            while (resultSet.next()) {
-                String employeeId = resultSet.getString("emp_id");
-                String firstName = resultSet.getString("first_name");
-                String lastName = resultSet.getString("last_name");
-                System.out.println(" Row: " + employeeId + " "
-                            + firstName + " " + lastName);
-            }
-  
-            System.out.println();
-  
-            ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
-            int columns = resultSetMetaData.getColumnCount();
-            String nameOne = resultSetMetaData.getColumnName(1);
-            String typeOne = resultSetMetaData.getColumnTypeName(1);
-            String labelOne = resultSetMetaData.getColumnLabel(1);
-            System.out.println(" Column count : " + columns);
-            System.out.println(" Column 1 name : " + nameOne);
-            System.out.println(" Column 1 type : " + typeOne);
-            System.out.println(" Column 1 label name : " + labelOne);
-  
-            System.out.println();
-  
-        } catch (SQLException sqlException) {
-            sqlException.printStackTrace();
-        } catch (Exception exception) {
-            System.err.println("General Error");
-            exception.printStackTrace();
-        } finally {
-            try {
-                if (resultSet != null) {
-                    resultSet.close();
-                }
-  
-                if (statement != null) {
-                    statement.close();
-                }
-  
-                if (connection != null) {
-                    connection.close();
-                }
-            } catch (SQLException sqlException) {
-                sqlException.printStackTrace();
-            } catch (Exception exception) {
-                exception.printStackTrace();
-            }
+        Object completedSearch;
+        
+        if (searchType == "1") {
+            completedSearch = searchById(searchTerm);
+        } else if (searchType == "2") {
+            completedSearch = searchByFirstName(searchTerm);
+        } else {
+            completedSearch = searchByLastName(searchTerm);
         }
+        
+        return completedSearch;
     }
     
     public void addEmployeeRecord() {
+        
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        
         try {
             Employee employee  = new Employee();
             
@@ -111,23 +62,24 @@ public class EmployeeDirectory {
             String roomNumber = employee.getRoomNumber();
             String phoneNumber = employee.getPhoneNumber();
             
-            connection = getConnection();
+            connection = getConnection(connection);
             
+            //Try normal also when time allows
             //statement = connection.createStatement();
             
             String queryString = "INSERT INTO employees "
             + "(first_name, last_name, ssn, dept, room, phone)"
             + "values ( ?, ?, ?, ?, ?, ? )";
             
-            PreparedStatement preparedQuery = connection.prepareStatement(queryString);
-            preparedQuery.setString(1, firstName);
-            preparedQuery.setString(2, lastName);
-            preparedQuery.setString(3, socialSecurityNumber);
-            preparedQuery.setString(4, department);
-            preparedQuery.setString(5, roomNumber);
-            preparedQuery.setString(6, phoneNumber);
+            statement = connection.prepareStatement(queryString);
+            statement.setString(1, firstName);
+            statement.setString(2, lastName);
+            statement.setString(3, socialSecurityNumber);
+            statement.setString(4, department);
+            statement.setString(5, roomNumber);
+            statement.setString(6, phoneNumber);
             
-            preparedQuery.executeQuery();
+            statement.executeQuery();
         } catch (SQLException sqlException) {
             sqlException.printStackTrace();
         } catch (Exception exception) {
@@ -154,7 +106,7 @@ public class EmployeeDirectory {
         }
     }
     
-    private Connection getConnection() {
+    private Connection getConnection(Connection connection) {
         try {
             Class.forName(properties.getProperty("db.driver"));
   
@@ -174,5 +126,162 @@ public class EmployeeDirectory {
         return connection;
     }
     
+    private Object searchById(String searchTerm) {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        Search search = new Search();
+        ResultSet resultSet = null;
+        
+        try {
+            connection = getConnection(connection);
+            
+            String queryString = "SELECT * FROM employees WHERE emp_id = ?";
+            
+            statement = connection.prepareStatement(queryString);
+            statement.setString(1, searchTerm);
+            
+            resultSet = statement.executeQuery();
+            
+            if (resultSet != null) {
+                search.setFoundEmployees(true);
+                
+                while(resultSet.next()) {
+                    System.out.println(resultSet.getString("emp_id"));
+                }
+            } else {
+                search.setFoundEmployees(false);
+            }
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        } catch (Exception exception) {
+            System.err.println("General Error");
+            exception.printStackTrace();
+        } finally {
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+  
+                if (statement != null) {
+                    statement.close();
+                }
+  
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException sqlException) {
+                sqlException.printStackTrace();
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+        }
+        
+        return search;
+    }
     
+    private Object searchByFirstName(String searchTerm) {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        Search search = new Search();
+        ResultSet resultSet = null;
+        
+        try {
+            connection = getConnection(connection);
+            
+            String queryString = "SELECT * FROM employees WHERE first_name = ?";
+            
+            statement = connection.prepareStatement(queryString);
+            statement.setString(1, searchTerm);
+            
+            resultSet = statement.executeQuery();
+            
+            if (resultSet != null) {
+                search.setFoundEmployees(true);
+                
+                while(resultSet.next()) {
+                    System.out.println(resultSet.getString("emp_id"));
+                }
+            } else {
+                search.setFoundEmployees(false);
+            }
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        } catch (Exception exception) {
+            System.err.println("General Error");
+            exception.printStackTrace();
+        } finally {
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+  
+                if (statement != null) {
+                    statement.close();
+                }
+  
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException sqlException) {
+                sqlException.printStackTrace();
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+        }
+        
+        return search;
+    }
+    
+    private Object searchByLastName(String searchTerm) {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        Search search = new Search();
+        ResultSet resultSet = null;
+        
+        try {
+            connection = getConnection(connection);
+            
+            String queryString = "SELECT * FROM employees WHERE last_name = ?";
+            
+            statement = connection.prepareStatement(queryString);
+            statement.setString(1, searchTerm);
+            
+            resultSet = statement.executeQuery();
+            
+            if (resultSet != null) {
+                search.setFoundEmployees(true);
+                
+                while(resultSet.next()) {
+                    System.out.println(resultSet.getString("emp_id"));
+                }
+            } else {
+                search.setFoundEmployees(false);
+            }
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        } catch (Exception exception) {
+            System.err.println("General Error");
+            exception.printStackTrace();
+        } finally {
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+  
+                if (statement != null) {
+                    statement.close();
+                }
+  
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException sqlException) {
+                sqlException.printStackTrace();
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+        }
+        
+        return search;
+    }
 }
